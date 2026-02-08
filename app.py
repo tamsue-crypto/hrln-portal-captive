9b23b52a8a20e8ab493b7480d6fd13fbade017f2
tamsue-crypto <mateusbarros45@yahoo.com>
2026-02-08 11:11:46 -0300

    Render Fixing Email

--- app.py
+++ app.py
@@ -40,8 +40,10 @@ google = oauth.register(
 
 # == Email ==
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_TIMEOUT"] = 10
 app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
 app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_USERNAME")
